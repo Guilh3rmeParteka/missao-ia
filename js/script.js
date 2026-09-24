d78f3b4cec1ca8404247b1032bcf5d464a1ec786
Guilh3rmeParteka <guilherme.parteka@escola.pr.gov.br>
2026-09-24 11:58:54 -0300

    save

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import {aleatorio} from ‘./aleatorio.js’;
-import {perguntas} from ‘./perguntas.js;
-const caixaPrincipal = document.querySelector(".caixa-principal");
+import {perguntas} from ‘./perguntas.js;const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
